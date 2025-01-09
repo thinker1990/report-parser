0ee16f941eed79a9719ba84ad682c2567e78b59d
Michael Liu <thinker1990@users.noreply.github.com>
2025-01-09 21:14:02 +0800

    Extract excel parser

--- main.py
+++ main.py
@@ -11,17 +11,21 @@ def read_root():
 
     Returns:
         dict: A dictionary containing a greeting message.
+
     """
     return {"Hello": "World"}
+
 
 @app.post("/parse/excel/")
 async def create_upload_file(file: UploadFile):
     """
     Asynchronously reads the content of an uploaded file and parses it.
+    
     Args:
         file (UploadFile): The file to be uploaded and parsed.
+    
     Returns:
         Parsed data from the file content.
+
     """
-    file_content = await file.read()
-    return parse_excel_v7(file_content)
+    return parse_excel_v7(await file.read())
